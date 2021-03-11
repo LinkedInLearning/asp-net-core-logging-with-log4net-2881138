526acc0a80456dc2dac9177abaa17a294e99e540
Ervis Trupja <ervistrupja@outlook.com>
2021-03-11 13:06:28 +0100

    Add project files.

---
+++ log4net.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace log4net.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
