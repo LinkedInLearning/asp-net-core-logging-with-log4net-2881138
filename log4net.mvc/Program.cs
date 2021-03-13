86f3b288ae2ec3d24d3e5e5cde0b92f7843134ad
Ervis Trupja <ervistrupja@outlook.com>
2021-03-13 17:13:51 +0100

    challenge solution

--- log4net.mvc/Program.cs
+++ log4net.mvc/Program.cs
@@ -21,6 +21,10 @@ namespace log4net.mvc
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                }).ConfigureLogging(builder => 
+                {
+                    builder.SetMinimumLevel(LogLevel.Information);
+                    builder.AddLog4Net("log4net.config");
                 });
     }
 }
