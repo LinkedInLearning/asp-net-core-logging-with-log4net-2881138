fc307ea44c30afebf9d0f6b15d63cf2c5e8eb55d
Ervis Trupja <ervistrupja@outlook.com>
2021-03-13 14:14:20 +0100

    log4net configured

--- log4net.console/Program.cs
+++ log4net.console/Program.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace log4net.console
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
+            //Load Configuration
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
             Console.WriteLine("Hello World!");
         }
     }
